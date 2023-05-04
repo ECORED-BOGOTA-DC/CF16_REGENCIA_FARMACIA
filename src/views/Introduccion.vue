7d6268fc17986be7a2a93596f063112e77d3c584
jorozcop <maechavarriaor@gmail.com>
2023-05-04 11:13:40 -0500

    ajustes intro y tema 1
    ajustes intro y tema 1

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,7 +8,30 @@
         span
           i.fas.fa-info
       h1 Introducción
-    
+
+    .row.mb-5
+      .col-lg-8
+        p El uso racional de medicamentos y dispositivos médicos es una responsabilidad no sólo del sector farmacéutico, sino de todos los actores del sistema general de salud; por tanto, la atención oportuna y segura durante la prescripción, dispensación y/o venta de medicamentos, productos cosméticos y dispositivos médicos, se relaciona directamente con las necesidades individuales y colectivas en un territorio especifico.
+
+        p En Colombia, se evidencia la inadecuada propagación de la automedicación y la venta de medicamentos sin prescripción médica, como una acción ligada a la tradición cultural; en este sentido el suministrador de tiendas y droguerías recomienda el uso indiscriminado de medicamentos, al igual que es muy común la recomendación por parte de familiares y amigos o vecinos.
+
+      .col-lg-4
+        figure.my-4
+          img(src="@/assets/curso/temas/introduccion/img1.svg", alt="alt") 
+
+
+    .row.mb-5
+      .col-lg-2
+        figure.my-4
+          img(src="@/assets/curso/temas/introduccion/img2.png", alt="alt") 
+
+      .col-lg-10.mb-5
+        p  Por todo lo anterior, se hace necesario priorizar estrategias orientadas al fomento de la cultura del uso adecuado de los medicamentos, la adherencia al tratamiento médico y la correcta disposición final de los medicamentos parcialmente utilizados o vencidos, tanto de uso humano como veterinario; esto con el fin de disminuir en parte  la resistencia a los antibióticos, las futuras alergias medicamentosas y la contaminación ambiental, situación que en el marco de la  salud pública, representa un alto grado de  alarma en Colombia y en el Mundo.
+
+        p Un punto de partida clave es la segura dispensación, informada y la atención satisfactoria en la entrega de productos farmacéuticos según protocolos y normatividad legal vigente.
+
+      p ¡Muchos éxitos en este proceso de aprendizaje!
+
 </template>
 
 <script>
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -5,7 +5,189 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
+      h1 Satisfacción del usuario
+
+    .row.mb-5
+      figure
+        img(src="@/assets/curso/temas/tema1/img1.svg", alt="alt")     
+
+    .row.mb-5
+      p La satisfacción en los servicios farmacéuticos y/o droguerías, se relaciona con varios factores como, por ejemplo:
+
+    .row.mb-5
+      .col-lg-4
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema1/img2.svg' alt='AvatarTop')
+          .tarjeta.tarjeta--morada
+            .py-4.px-3
+              p.mb-0 Las condiciones de vida.
+
+      .col-lg-4
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema1/img3.svg' alt='AvatarTop')
+          .tarjeta.tarjeta--morada
+            .py-4
+              p.mb-0 Las prácticas y experiencias previas.
+
+      .col-lg-4
+        .tarjeta-avatar-b.mb-5
+          .tarjeta-avatar-b__img
+            img(src='@/assets/curso/temas/tema1/img4.svg' alt='AvatarTop')
+          .tarjeta.tarjeta--morada
+            .py-4.px-3
+              p.mb-0 Las esperanzas del futuro. 
+
+    .row.mb-5
+      p.mb-4 Por lo anterior, se concluye que el nivel de satisfacción de un paciente, también denominado usuario o cliente, es una respuesta subjetiva y parcializada, puesto que se basa en cómo ha sido atendido, cómo han atendido a la familia y amigos y cómo espera que le atiendan.
+
+      p No obstante, desde la gestión de respuestas oportunas y optimización de los recursos, posterior a la compra o adquisición de un producto o servicio, los usuarios/clientes experimentan uno de estos tres niveles de satisfacción:
+
+
+    .row.mb-5
+      .col-lg-4.mb-4
+        .tarjeta.tarjeta--azul2.p-4
+          .row.justify-content-center
+            .col-6.col-lg-5
+              figure.mb-4
+                img(src="@/assets/curso/temas/tema1/img5.svg", alt="alt")
+            
+          h5.text-center Insatisfacción
+          p.text-center La insatisfacción es experimentada por el usuario cuando el producto o servicio NO cumple con las expectativas.
+
+      .col-lg-4.mb-4
+        .tarjeta.tarjeta--azul2.p-4
+          .row.justify-content-center
+            .col-6.col-lg-5
+              figure.mb-4
+                img(src="@/assets/curso/temas/tema1/img6.svg", alt="alt")
+          
+          h5.text-center Satisfacción
+          p.text-center La Satisfacción del usuario se refleja cuando el producto o servicio se limita a cumplir con las expectativas.
+
+      .col-lg-4.mb-4
+        .tarjeta.tarjeta--azul2.p-4
+          .row.justify-content-center
+            .col-6.col-lg-5
+              figure.mb-4
+                img(src="@/assets/curso/temas/tema1/img7.svg", alt="alt")
+          
+          h5.text-center La complacencia
+          p.text-center Se trata del estado máximo de satisfacción, por encima de lo esperado, cuando el producto o servicio supera las expectativas.
+
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .bloque-texto-g.color-acento-contenido.p-3.p-sm-4.p-md-4
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/temas/tema1/img8.svg')})`}"
+          )
+          .bloque-texto-g__texto.p-4
+            p.mb-0 Dependiendo el nivel de satisfacción del usuario, se identifica el grado de lealtad hacia una marca o empresa, por ejemplo: un cliente insatisfecho cambiará de marca o proveedor de inmediato; el cliente satisfecho se mantendrá leal; pero, tan solo hasta que encuentre otro proveedor que tenga una mejor oferta; pero un usuario complacido será leal a una marca porque siente afinidad emocional que supera ampliamente a una preferencia racional.
+
+    separador
+
+    #t_1_1.titulo-segundo.color-acento-contenido
+      h2 1.1  Atención para la satisfacción
+
+    .row.mb-5
+      .col-lg-3
+        figure.mb-4
+          img(src="@/assets/curso/temas/tema1/img9.svg", alt="alt")
+
+      .col-lg-9
+        p Cualquier servicio de atención a clientes pretende, no solo lograr generar utilidades y mejoras a la organización sino impactar de manera positiva y transformadora en sus usuarios, sus vidas y dinámicas. Tanto así que la atención a usuarios, en el sector farmacéutico y de servicios de salud, cuenta con distintos esquemas o protocolos propios para favorecer el cumplimiento de tales propósitos.
+
+        p En el siguiente recurso, se mostrará los atributos del buen servicio, en la atención a usuarios de salud. Se debe Visualizar conscientemente el recurso y apropiar los aspectos conceptuales que allí se ofrecen. ¡Adelante!
+
+    .row.my-5(data-aos="fade-zoom")
+      figure
+        .video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/11111111" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+    separador
+
+    #t_1_2.titulo-segundo.color-acento-contenido
+      h2 1.2  Diseño de las instalaciones
+
+    .row.mb-5
+      p Un servicio a clientes que se caracterice por ser efectivo, respetuoso, oportuno e integral, además de convertirse en una experiencia de crecimiento y consolidación para la empresa u organización, es factor de beneficios multidireccionales, en donde el cliente, las familias de los clientes, otras empresas u organizaciones, la sociedad y el mercado, se ven positivamente impactados.
+
+      p Los resultados de la satisfacción del usuario, gracias a un servicio farmacéutico de calidad son:
+
+    .row.mb-5
+      .col-lg-4.mb-4
+        .tarjeta.tarjeta--azul3.p-4
+          .row.justify-content-center
+            .col-6.col-lg-5
+              figure.mb-4
+                img(src="@/assets/curso/temas/tema1/img10.svg", alt="alt")
+            
+          h5.text-center Lealtad
+          p.text-center El usuario satisfecho, por lo general, vuelve a comprar; por tanto, se obtiene como beneficio, su lealtad.
+
+      .col-lg-4.mb-4
+        .tarjeta.tarjeta--azul3.p-4
+          .row.justify-content-center
+            .col-6.col-lg-5
+              figure.mb-4
+                img(src="@/assets/curso/temas/tema1/img11.svg", alt="alt")
+          
+          h5.text-center Difusión gratuita del servicio
+          p.text-center El cliente satisfecho comunica a otros su experiencia positiva. Se obtiene, entonces, difusión gratuita ya que compartirá su experiencia con familiares, amistades, conocidos, entre otros.
+
+      .col-lg-4.mb-4
+        .tarjeta.tarjeta--azul3.p-4
+          .row.justify-content-center
+            .col-6.col-lg-5
+              figure.mb-4
+                img(src="@/assets/curso/temas/tema1/img12.svg", alt="alt")
+          
+          h5.text-center Posicionamiento
+          p.text-center El usuario satisfecho deja a un lado a la competencia.
+
+
+    separador
+
+    #t_1_3.titulo-segundo.color-acento-contenido
+      h2 1.3  Información y formación mediante la atención
+
+    .row.mb-5
+      p El buen servicio al cliente ha de estar siempre presente en cada tipo de atención que solicite el usuario. Debe, desde luego, prevalecer la interacción con él, incluso con los suyos si fuera el caso. Esto, desde el saludo hasta el final de la atención del paciente y su posterior egreso. Para ello, se requiere un proceso de capacitación y motivación permanentes, no sólo al personal que está en contacto con el usuario sino, en general, a todo el talento humano de la organización. Esto favorece lograr la prestación de un servicio con calidad.
+
+      p La entrega responsable de medicamentos y/o dispositivos médicos, tiene algunas obligaciones y responsabilidades de orden humano, institucional, normativo legal, entre otras. A continuación, se muestran algunas obligaciones basadas en los protocolos de atención al ciudadano, en el sector salud:
+
+    .row.mb-5
+      .tarjeta--morada2.py-4.px-5
+        .row 
+          .col-8.col-lg-3
+            figure.mt-5
+              img(src="@/assets/curso/temas/tema1/img13.svg", alt="alt")
+
+          .col-lg-9
+            ul.lista-ul--color
+              li 
+                i.fas.fa-check
+                | Exigir fórmula médica para medicamentos en los cuales el laboratorio productor así lo exija.            
+              li 
+                i.fas.fa-check
+                | Verificar que las preparaciones magistrales estériles, relacionen en el rótulo o etiqueta la información sobre el nombre del paciente hospitalizado o ambulatorio; nombre de la mezcla o fórmula y firma del responsable.            
+              li 
+                i.fas.fa-check
+                | Fomentar en los servicios hospitalarios la relevancia de la reconciliación medicamentos.            
+              li 
+                i.fas.fa-check
+                | No dispensar, si se evidencia posibles errores en la prescripción médica.            
+              li 
+                i.fas.fa-check
+                | Verificar y controlar que los medicamentos dispensados correspondan a los prescritos o formulados por el médico tratante.            
+              li 
+                i.fas.fa-check
+                | Realizar la dispensación informada al usuario: almacenamiento, restitución, medidas y dosis, etc. Educar al usuario sobre efectos posibles del medicamento.            
+              li 
+                i.fas.fa-check
+                | Fomentar el uso adecuado de los medicamentos de venta sin prescripción o de venta libre.            
+
       
 </template>
 
